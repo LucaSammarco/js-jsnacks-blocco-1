5ec56d1186e76c9914d2feb71bdeb917506fa3f8
Il tuo nome <la_tua_email@example.com>
2024-05-13 12:03:30 +0200

    setting up second snack

---
+++ Snack 2/js/script.js
@@ -0,0 +1,14 @@
+// //L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+// const numOne = prompt ("First Number");
+// const numTwo = prompt ("Second Number");
+
+// console.log(Math.max(numOne, numTwo));
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+const partyMembers = ["Luca", "Giovanni", "Michela", "Francesco", "Laura", "Teresa", "Alice" ]
+
+const userInvite = prompt ("Check your name on the list")
