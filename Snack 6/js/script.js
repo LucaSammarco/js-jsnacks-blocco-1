500b9f74cc60bf37d005729cd876cb34ddd3bbc8
Il tuo nome <la_tua_email@example.com>
2024-05-14 10:54:20 +0200

    corrected

---
+++ Snack 6/js/script.js
@@ -0,0 +1,13 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+
+const nums = [1, 3, 2, 9, 8]
+
+for (let index = 0; index < nums.length; index++) {
+    if (index % === 1) {
+        somma += nums[index];
+    }
+    
+}
+
+console.log(somma)
