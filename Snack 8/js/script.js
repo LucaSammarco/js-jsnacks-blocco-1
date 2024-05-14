0c1bd8df7ab20741c44b6b72bdc438a25667c6fa
Il tuo nome <la_tua_email@example.com>
2024-05-14 11:36:09 +0200

    creating element

--- Snack 8/js/script.js
+++ Snack 8/js/script.js
@@ -1,2 +1,6 @@
 // Snack8 
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+
+const div = document.createElement('div');
+document.getElementById('snack8').appendChild(div);
