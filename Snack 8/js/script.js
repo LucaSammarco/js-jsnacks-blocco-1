c831b4a4d72431a2ca5c594876e10bb9253199b4
Il tuo nome <la_tua_email@example.com>
2024-05-14 11:43:33 +0200

    create 2 div + class

--- Snack 8/js/script.js
+++ Snack 8/js/script.js
@@ -2,5 +2,13 @@
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
 
 
-const div = document.createElement('div');
-document.getElementById('snack8').appendChild(div);
+const snack8 = document.getElementById('snack8');
+const divPari = document.createElement('div');
+snack8.appendChild(divPari);
+divPari.classList.add("verdi");
+
+
+
+const divDispari = document.createElement('div');
+snack8.appendChild(divDispari);
+divDispari.classList.add("rossi");
